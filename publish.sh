12e759f6030c8c2fc85bf069445de6a1ed5135be
Guido Pleßmann <gplssm@posteo.de>
2025-07-07 12:48:30 +0200

    feat: publish to manitu and clean dest dir

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-hugo build
-rsync -avz -e 'ssh -p 22793' public/ admin@gplssm.dynu.net:/mnt/raid\ storage/apps-data/gruenzschocher-website/applications-gruenzschocher-website-pvc-\*
+hugo build --cleanDestinationDir
+rsync -avz --delete public/ ssh300008666@ngcobalt394.manitu.net:/home/sites/site100019389/web/gruenzschocher.de
