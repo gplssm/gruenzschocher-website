bca3a52da3bbc65e3aabc830e0a129e6f716f167
Guido Pleßmann <gplssm@posteo.de>
2025-07-09 07:30:29 +0200

    feat:  parametrize publish script and describe in README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Publish to web server
+
+```bash
+SSH_USER=<ssh user> WEB_SERVER=<web server> WWW_PATH=<www path> ./publish.sh
+```
--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 hugo build --cleanDestinationDir
-rsync -avz --delete public/ ssh300008666@ngcobalt394.manitu.net:/home/sites/site100019389/web/gruenzschocher.de
+rsync -avz --delete public/ ${SSH_USER}@${WEB_SERVER}:${WWW_PATH}
